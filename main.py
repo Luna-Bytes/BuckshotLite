30748c29495a3aacad46581b1eb21a5fc026ca54
LunaBytes <luna@schleichy.de>
2026-07-08 17:49:13 +0200

    removed comments in main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# This is a sample Python script.
 from classes.Shotgun import Shotgun
-
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 def main():
     print("Buckshot Roulette Lite")
@@ -15,9 +10,5 @@ def main():
     print(shotgun)
 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
