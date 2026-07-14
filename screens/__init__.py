9bd9367ec9bc50f6c6244a2f45e5b9be3a463e37
LunaBytes <luna@schleichy.de>
2026-07-14 15:39:09 +0200

    deleted __init__.py

--- screens/__init__.py
+++
@@ -1,8 +0,0 @@
-# Empty file or optionally re-export your screens
-from .MenuScreen import MenuScreen
-from .SettingsScreen import SettingsScreen
-from .NewGameScreen import NewGameScreen
-
-__all__ = ["MenuScreen", "SettingsScreen", "NewGameScreen"]
-
-
